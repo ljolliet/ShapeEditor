4094e3a08e48dad8c0e4c1a19ea6a679f3ebe369
paulvigneau <paulvigneau98@gmail.com>
2020-03-24 12:04:14 +0100

    Rename Polygon class

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,11 +1,11 @@
 import utils.Color;
 import utils.Vec2D;
 
-public class Polygone extends SimpleShape {
+public class Polygon extends SimpleShape {
     private int nbSide;
     private double sideLength;
 
-    public Polygone(int nbSide, double sideLength, Vec2D position, Color color, Vec2D rotationCenter, double rotation) {
+    public Polygon(int nbSide, double sideLength, Vec2D position, Color color, Vec2D rotationCenter, double rotation) {
         super(position, color, rotationCenter, rotation);
         this.nbSide = nbSide;
         this.sideLength = sideLength;
