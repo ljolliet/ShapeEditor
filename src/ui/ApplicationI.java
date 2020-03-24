168c49559601b8a57b5abd4429f5a5ee5c3fd9e1
paulvigneau <paulvigneau98@gmail.com>
2020-03-24 19:45:32 +0100

    Beginning of UI

--- src/ui/ApplicationI.java
+++ src/ui/ApplicationI.java
@@ -2,8 +2,13 @@ package ui;
 
 public interface ApplicationI {
 
+    String WINDOW_TITLE = "ShapeEditor";
     int WINDOW_HEIGHT = 600;
     int WINDOW_WIDTH  = 800;
+
+    int TOOLBAR_WIDTH = 100;
+
+    int OPTION_HEIGHT = 50;
 
     // TODO Add some methods to communicate with the editor
 }
--- src/ui/JavaFXApp.java
+++ src/ui/JavaFXApp.java
@@ -1,6 +1,11 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXApp extends Application implements ApplicationI {
@@ -9,6 +14,53 @@ public class JavaFXApp extends Application implements ApplicationI {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setResizable(false);
 
+//        Stage
+//        |  Scene
+//        |  |  VBox (windowLayout)
+//        |  |  |  HBox (optionLayout)
+//        |  |  |  HBox (editorLayout)
+//        |  |  |  |  VBox (toolbarBox)
+//        |  |  |  |  Group (root)
+//        |  |  |  |  |  Canvas
+
+        VBox windowLayout = new VBox();
+
+        // Top option bar
+        HBox optionLayout = new HBox();
+        optionLayout.setPrefHeight(OPTION_HEIGHT);
+        optionLayout.setPrefWidth(WINDOW_WIDTH);
+        optionLayout.setStyle("-fx-background-color: darkred");
+        windowLayout.getChildren().add(optionLayout);
+
+        // Editor layout
+        HBox editorLayout = new HBox();
+        editorLayout.setPrefHeight(WINDOW_HEIGHT - OPTION_HEIGHT);
+        editorLayout.setPrefWidth(WINDOW_WIDTH);
+        windowLayout.getChildren().add(editorLayout);
+
+        // Toolbar
+        VBox toolbarBox = new VBox();
+        toolbarBox.setPrefHeight(WINDOW_HEIGHT);
+        toolbarBox.setPrefWidth(TOOLBAR_WIDTH);
+        toolbarBox.setStyle("-fx-background-color: lightgray");
+        editorLayout.getChildren().add(toolbarBox);
+
+        // Scene layout
+        Group root = new Group();
+        editorLayout.getChildren().add(root);
+
+        Canvas canvas = new Canvas();
+        canvas.setWidth(WINDOW_WIDTH - TOOLBAR_WIDTH);
+        canvas.setHeight(WINDOW_HEIGHT - OPTION_HEIGHT);
+        root.getChildren().add(canvas);
+
+
+
+        Scene scene = new Scene(windowLayout, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
