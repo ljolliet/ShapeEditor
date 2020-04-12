6773bc8e50549db382bd74ca1e406f7ad5d4952e
paulvigneau <paulvigneau98@gmail.com>
2020-04-12 12:16:09 +0200

    Modify ShapeObservable class

--- src/editor/ShapeObservable.java
+++ src/editor/ShapeObservable.java
@@ -1,30 +1,23 @@
 package editor;
 
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 
 public abstract class ShapeObservable implements Shape {
-    private List<ShapeObserverI> observersOrdered = new LinkedList<ShapeObserverI>();
-    private Set<ShapeObserverI> observersSet = new HashSet<ShapeObserverI>();
+    private Set<ShapeObserverI> observers = new HashSet<>();
 
     public void addObserver(ShapeObserverI so) {
-        if (!observersSet.contains(so)) {
-            observersOrdered.add(so);
-            observersSet.add(so);
-        }
+        this.observers.add(so);
     }
 
     public void removeObserver(ShapeObserverI so) {
-        observersOrdered.remove(so);
-        observersSet.remove(so);
+        this.observers.remove(so);
     }
 
     public void notifyObservers() {
-        Object[] copy = observersOrdered.toArray();
-        for (Object u : copy)
-            ((ShapeObserverI) u).update(this);
+        Set<ShapeObserverI> copy = new HashSet<>(this.observers);
+        for (ShapeObserverI u: copy)
+            u.update(this);
     }
 
     @Override
