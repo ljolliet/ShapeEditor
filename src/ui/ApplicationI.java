9ca306685e0ac8ba1006627d16ab6dffd5b77b39
paulvigneau <paulvigneau98@gmail.com>
2020-04-20 11:05:44 +0200

    Refacto JavaFXApp and beginning of Drag&Drop

--- src/ui/ApplicationI.java
+++ src/ui/ApplicationI.java
@@ -3,7 +3,7 @@ package ui;
 public interface ApplicationI {
 
     String WINDOW_TITLE = "ShapeEditor";
-    int WINDOW_HEIGHT = 600;
+    int WINDOW_HEIGHT = 650;
     int WINDOW_WIDTH  = 800;
 
     int TOOLBAR_WIDTH = 100;
--- src/ui/JavaFXApp.java
+++ src/ui/JavaFXApp.java
@@ -18,42 +18,69 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JavaFXApp extends Application implements ApplicationI {
 
     private Rendering rendering;
+    private Editor editor;
 
     private Group root;
     private VBox toolbarBox;
+    private BorderPane borderPane;
+    private ImageView trashImage;
+
+    // Temporary
+    private Rectangle rec;
+    private Group grp;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle(WINDOW_TITLE);
         primaryStage.setResizable(false);
 
-        Editor editor = Editor.getInstance();
+        this.editor = Editor.getInstance();
 
 /*
         Stage
         |  Scene
-        |  |  VBox (windowLayout)
-        |  |  |  HBox (optionLayout)
-        |  |  |  HBox (editorLayout)
-        |  |  |  |  BorderPane (toolbarRoot)
-        |  |  |  |  |   VBox (toolbarBox)
-        |  |  |  |  Group (root)
-        |  |  |  |  |  Canvas
+        |  |  StackPane
+        |  |  |  VBox (windowLayout)
+        |  |  |  |  HBox (optionLayout)
+        |  |  |  |  HBox (editorLayout)
+        |  |  |  |  |  BorderPane (toolbarRoot)
+        |  |  |  |  |  |   VBox (toolbarBox)
+        |  |  |  |  |  |   ImageView (trashIm)
+        |  |  |  |  |  Group (root)
+        |  |  |  |  |  |  Canvas
+        |  |  |  Group
+        |  |  |  |  Canvas
 */
 
         VBox windowLayout = new VBox();
-        Scene scene = new Scene(windowLayout);
+        StackPane stackPane = new StackPane(windowLayout);
+        Scene scene = new Scene(stackPane);
         primaryStage.setScene(scene);
+
+        // Temporary
+        // Shadow shape when drag & drop
+        rec = new Rectangle(100,100);
+        // Create a group to move the shadow shape inside
+        grp = new Group(new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT));
+        // The group is completely transparent to mouse events
+        grp.setMouseTransparent(true);
+        stackPane.getChildren().add(grp);
+
+        windowLayout.toFront();
 
         // Top option bar
         HBox optionLayout = new HBox();
@@ -105,12 +132,12 @@ public class JavaFXApp extends Application implements ApplicationI {
         // Editor layout
         HBox editorLayout = new HBox();
         //editorLayout.setPrefHeight(WINDOW_HEIGHT - OPTION_HEIGHT);
-        editorLayout.setPrefWidth(WINDOW_WIDTH);
+        //editorLayout.setPrefWidth(WINDOW_WIDTH);
         windowLayout.getChildren().add(editorLayout);
 
         //Toolbar root
-        BorderPane borderPane = new BorderPane();
-        borderPane.setPrefHeight(WINDOW_HEIGHT);
+        this.borderPane = new BorderPane();
+        //borderPane.setPrefHeight(WINDOW_HEIGHT);
         borderPane.setPrefWidth(TOOLBAR_WIDTH);
         borderPane.setStyle("-fx-background-color: lightgray");
         // Toolbar
@@ -122,12 +149,12 @@ public class JavaFXApp extends Application implements ApplicationI {
 
         //trash
         //TODO refacto
-        ImageView trashim = new ImageView( new Image(getClass().getClassLoader().getResource("trash.png").toString()));
-        trashim.setPreserveRatio(true);
-        trashim.setFitHeight(TRASH_HEIGHT);
-        borderPane.setBottom(trashim);
-        BorderPane.setAlignment(trashim, Pos.CENTER);
-        BorderPane.setMargin(trashim, new Insets(TOOLBAR_SPACING));
+        this.trashImage = new ImageView(new Image(getClass().getClassLoader().getResource("trash.png").toString()));
+        trashImage.setPreserveRatio(true);
+        trashImage.setFitHeight(TRASH_HEIGHT);
+        borderPane.setBottom(trashImage);
+        BorderPane.setAlignment(trashImage, Pos.CENTER);
+        BorderPane.setMargin(trashImage, new Insets(TOOLBAR_SPACING));
         editorLayout.getChildren().add(borderPane);
 
         // Scene layout
@@ -139,127 +166,17 @@ public class JavaFXApp extends Application implements ApplicationI {
         canvas.setHeight(SCENE_HEIGHT);
         root.getChildren().add(canvas);
 
-        //toolbar to scene drag and drop
-        toolbarBox.setOnDragDetected(event -> {
-            System.out.println("drag detected");
-            toolbarBox.startFullDrag();
+        /* Set events */
+        setToolbarEvents();
+        setSceneEvents();
 
-            if (event.getButton() == MouseButton.PRIMARY) {
-                int i = 0;
-                for (Node node : toolbarBox.getChildren()) {
-                    if (event.getPickResult().getIntersectedNode() == node) {
-                        editor.setShapeDragged(editor.getToolbar().getShapes().get(i));
-                        break;
-                    }
-                    i++;
-                }
-            }
-
-            event.consume();
+        // Event on stack pane
+        stackPane.setOnMouseDragged(event -> {
+            System.out.println("x = " + event.getX() + " ; y = " + event.getY());
+            rec.setX(event.getX());
+            rec.setY(event.getY());
         });
 
-        //drop in trash
-        borderPane.setOnMouseReleased(mouseEvent -> {
-            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-                editor.removeShapeFromToolbar(editor.getShapeDragged());
-            }
-        });
-
-        toolbarBox.setOnMouseDragged(event -> {
-            //TODO display shape
-            event.consume();
-        });
-
-        root.setOnMouseDragReleased(event -> {
-            System.out.println("drag released");
-
-            // create a shape
-            if (editor.getShapeDragged() != null)
-                try {
-                    Shape newShape = editor.getShapeDragged().clone();
-                    newShape.setPosition(new Point2D(event.getX(), event.getY()));
-                    editor.addShapeToScene((ShapeObservable) newShape);
-                    editor.setShapeDragged(null);
-                } catch (CloneNotSupportedException e) {
-                    e.printStackTrace();
-                }
-
-            event.consume();
-        });
-
-        /* scene to scene drag and drop */
-
-        this.root.setOnMousePressed(event -> {
-            rendering.hideEditionDialog();
-            if (event.getButton() == MouseButton.PRIMARY) {
-                System.out.println("mouse pressed");
-                boolean inShape = false;
-                //click on an existing shape
-                for (Shape s : editor.getScene().getShapes()) {
-                    if (s.contains(new Point2D(event.getX(), event.getY()))) {
-                        System.out.println("found: " + s);
-                        root.setCursor(Cursor.CLOSED_HAND);
-                        editor.setShapeDragged(s);
-                        inShape = true;
-                        break;
-                    }
-                }
-                // selecte shapes
-                if(!inShape){
-                    editor.getSelectionShape().setSelectionStartPoint(new Point2D(event.getX(), event.getY()));
-                }
-            }
-            event.consume();
-        });
-
-        this.root.setOnMouseDragged(mouseEvent -> {
-            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-                if (editor.getShapeDragged() != null) {
-                    editor.getShapeDragged().setPosition(new Point2D(mouseEvent.getX(), mouseEvent.getY()));
-                } else {
-                    editor.getSelectionShape().setSelectionEndPoint(new Point2D(mouseEvent.getX(), mouseEvent.getY()));
-                    rendering.drawSelectionFrame();
-                }
-            }
-        });
-
-        this.root.setOnMouseReleased(mouseEvent -> {
-            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-                System.out.println("mouse released");
-                rendering.drawEditor();
-                ArrayList<ShapeObservable> selectedShapes = new ArrayList<>();
-                if (editor.getSelectionShape() != null)
-                    for (ShapeObservable s : editor.getScene().getShapes())
-                        if (s.contained(editor.getSelectionShape())) {
-                            selectedShapes.add(s);
-                            System.out.println(s + " selected");
-                        }
-                editor.getScene().setSelectedShapes(selectedShapes);
-
-                root.setCursor(Cursor.DEFAULT);
-                editor.setShapeDragged(null);
-                rendering.drawEditor();
-            }
-            mouseEvent.consume();
-        });
-
-        this.root.setOnMouseClicked(e -> {
-            rendering.hideEditionDialog();
-            if (e.getButton() == MouseButton.SECONDARY) {
-                if(editor.getScene().getSelectedShapes().size() != 0){
-                    System.out.println("right click on selection");
-                }
-                else
-                    for(Shape s : editor.getScene().getShapes()){
-                        if(s.contains(new Point2D(e.getX(), e.getY()))) {
-                            System.out.println("right click on shape");
-                            EditionDialogI ed = s.createEditionDialog();
-                            ed.setPosition(new Point2D(e.getScreenX(), e.getScreenY()));
-                            ed.draw(rendering);
-                        }
-                    }
-            }
-        });
 //          TODO : to group
 //        MenuItem groupShape = new MenuItem("Group");
 //        groupShape.setOnAction(e -> {
@@ -279,5 +196,202 @@ public class JavaFXApp extends Application implements ApplicationI {
         rendering.drawEditor();
 
         primaryStage.show();
+    }
+
+    private void setToolbarEvents() {
+        toolbarBox.setOnDragDetected(event -> {
+            System.out.println("[TOOLBAR] Drag detected");
+            // Start drag event
+            toolbarBox.startFullDrag();
+
+            // Detect left click
+            if (event.getButton() == MouseButton.PRIMARY) {
+                int i = 0;
+                // Search for shape selected
+                for (Node node : toolbarBox.getChildren()) {
+                    if (event.getPickResult().getIntersectedNode() == node) {
+                        editor.setShapeDragged(editor.getToolbar().getShapes().get(i)); // Found
+                        break;
+                    }
+                    i++;
+                }
+            }
+
+            event.consume();
+        });
+        // Empty
+        /*toolbarBox.setOnMouseDragged(event -> {
+            //TODO display shape
+            event.consume();
+        });*/
+        // Empty
+        /*toolbarBox.setOnMouseDragReleased(event -> {
+            System.out.println("[TOOLBAR] Drag released");
+
+            event.consume();
+        });*/
+
+        borderPane.setOnMouseReleased(mouseEvent -> {
+            System.out.println("[BORDERPANE] Mouse released");
+
+            // Detect left click
+            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+                // If release over trash --> deleted selected shape
+                if (mouseEvent.getPickResult().getIntersectedNode() == this.trashImage) {
+                    editor.removeShapeFromToolbar(editor.getShapeDragged());
+                    editor.removeShapeFromScene((ShapeObservable) editor.getShapeDragged());
+                }
+            }
+        });
+    }
+
+    private void setSceneEvents() {
+        // Empty
+        /*root.setOnDragDetected(event -> {
+            root.startFullDrag();
+            System.out.println("[ROOT] Drag detected");
+
+            event.consume();
+        });*/
+
+        root.setOnMouseDragged(event -> {
+            // Detect left click
+            if (event.getButton() == MouseButton.PRIMARY) {
+
+                // If there is a shape to drag
+                if (editor.getShapeDragged() != null) {
+
+                    // If mouse is outside the scene
+                    if (!grp.getChildren().contains(rec) && (event.getX() < 0 || event.getY() < 0)) {
+                        // Create shadow shape for dropping in toolbar
+                        grp.getChildren().add(rec);
+                        grp.toFront();
+                    }
+                    // Inside the scene
+                    else if (event.getX() > 0 && event.getY() > 0) {
+                        // Update shape position
+                        editor.getShapeDragged().setPosition(new Point2D(event.getX(), event.getY()));
+
+                        // Hide shadow shape
+                        if (grp.getChildren().contains(rec)) {
+                            grp.getChildren().remove(rec);
+                            grp.toBack();
+                        }
+                    }
+                }
+                // Else, it is a select action
+                else {
+                    editor.getSelectionShape().setSelectionEndPoint(new Point2D(event.getX(), event.getY()));
+                    rendering.drawSelectionFrame();
+                }
+            }
+
+            event.consume();
+        });
+
+        root.setOnMouseDragReleased(event -> {
+            System.out.println("[ROOT] Drag released");
+
+            // If there is a shape dragged
+            if (editor.getShapeDragged() != null)
+                try {
+                    // Clone the shape and paste it to the scene
+                    Shape newShape = editor.getShapeDragged().clone();
+                    newShape.setPosition(new Point2D(event.getX(), event.getY()));
+                    editor.addShapeToScene((ShapeObservable) newShape);
+                    editor.setShapeDragged(null);
+                } catch (CloneNotSupportedException e) {
+                    e.printStackTrace();
+                }
+
+            event.consume();
+        });
+
+        root.setOnMousePressed(event -> {
+            // Close edition dialog
+            rendering.hideEditionDialog();
+
+            // Detect left click
+            if (event.getButton() == MouseButton.PRIMARY) {
+                System.out.println("[ROOT] Mouse pressed");
+
+                boolean inShape = false;
+                // Look if the mouse is on an existing shape
+                for (Shape s : editor.getScene().getShapes()) {
+                    if (s.contains(new Point2D(event.getX(), event.getY()))) { // Found
+                        root.setCursor(Cursor.CLOSED_HAND);
+                        editor.setShapeDragged(s);
+                        inShape = true;
+                        break;
+                    }
+                }
+                // If no shape found --> start select action
+                if (!inShape)
+                    editor.getSelectionShape().setSelectionStartPoint(new Point2D(event.getX(), event.getY()));
+            }
+
+            event.consume();
+        });
+
+        root.setOnMouseReleased(event -> {
+            System.out.println("[ROOT] Mouse released");
+
+            // Detect left click
+            if (event.getButton() == MouseButton.PRIMARY) {
+                // Hide shadow shape
+                grp.getChildren().remove(rec);
+                grp.toBack();
+
+                // If it is a select action
+                if (editor.getSelectionShape() != null) {
+                    ArrayList<ShapeObservable> selectedShapes = new ArrayList<>();
+
+                    // Add all selected shapes in array
+                    for (ShapeObservable s : editor.getScene().getShapes())
+                        if (s.contained(editor.getSelectionShape())) {
+                            selectedShapes.add(s);
+                            System.out.println(s + " selected");
+                        }
+
+                    // Save selected shapes
+                    editor.getScene().setSelectedShapes(selectedShapes);
+                }
+
+                root.setCursor(Cursor.DEFAULT);
+                editor.setShapeDragged(null);
+                rendering.drawEditor();
+            }
+
+            event.consume();
+        });
+
+        root.setOnMouseClicked(e -> {
+            System.out.println("[ROOT] Mouse clicked");
+
+            // Close edition dialog
+            rendering.hideEditionDialog();
+
+            // Detect right click
+            if (e.getButton() == MouseButton.SECONDARY) {
+                // Click on a selection
+                if (editor.getScene().getSelectedShapes().size() != 0) {
+                    System.out.println("right click on selection");
+                }
+                // Click on a single shape
+                else {
+                    // Look for the clicked shape
+                    for (Shape s : editor.getScene().getShapes()) {
+                        if (s.contains(new Point2D(e.getX(), e.getY()))) { // Found
+                            System.out.println("right click on shape");
+                            // Create edition dialog
+                            EditionDialogI ed = s.createEditionDialog();
+                            ed.setPosition(new Point2D(e.getScreenX(), e.getScreenY()));
+                            // Draw it
+                            ed.draw(rendering);
+                        }
+                    }
+                }
+            }
+        });
     }
 }
